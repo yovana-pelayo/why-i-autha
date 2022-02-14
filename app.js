7497ba12a7a358293d395f8c06f56846eb7b0d4f
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-02-14 11:52:40 -0800

    my redirect function is working. Email sign up is working. Sign in is still not working.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { redirectIfLoggedIn, signupUser } from './fetch-utils.js';
+
 const signInForm = document.getElementById('sign-in');
 const signInEmail = document.getElementById('sign-in-email');
 const signInPassword = document.getElementById('sign-in-password');
@@ -9,3 +11,9 @@ const signUpPassword = document.getElementById('sign-up-password');
 // Wire up sign in and sign up forms to supabase
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
+signUpForm.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    console.log(signUpEmail.value, signUpPassword.value);
+    await signupUser(signUpEmail.value, signUpPassword.value);
+    redirectIfLoggedIn();
+});
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://swgiucfleoaonawjlvme.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3Z2l1Y2ZsZW9hb25hd2psdm1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ0NTIxNjEsImV4cCI6MTk2MDAyODE2MX0.KwPiwmAFu_q_7yxOSUYv4k6Ejgu8uDsvfsGLixOpD9o';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -8,12 +8,22 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signupUser(email, password) {
+    const resp = await client.auth.signUp({ email, password });
+    console.log(resp);
+}
 
-export async function signInUser(email, password) {}
+export async function signInUser(email, password) {
+    const resp = await client.auth.signIn({ email, password });
+    console.log(resp);
 
+}
 export async function checkAuth() {}
 
 export async function redirectIfLoggedIn() {}
 
 export async function logout() {}
+
+function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}
